d98a7fac0b63ffaf7e30b1af692d7e0be7791cbc
amina294 <aminarufai294@yahoo.com>
2022-09-25 10:27:42 +0100

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return 0
     average = 0
     div = 0
-    for tuple in my_list:
-        average += tuple[0] * tuple[1]
+    for tup in my_list:
+        average += tup[0] * tup[1]
         div += tup[1]
     return float(average / div)    
